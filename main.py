b04f8a1d1b69eea7d2fae90d81ef7cc49ca46daf
steff121 <100987012+steff121@users.noreply.github.com>
2023-03-06 16:36:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,81 +6,35 @@ import re
 
 interface = tk.Tk()
 interface.title("Prototype")  # Interface title
-interface.geometry("1000x700")  # Interface size
+interface.geometry("1020x720")  # Interface size
 
-
+def showPassword():
+    if enter.cget('show') == '*':
+        enter.config(show='')
+    else:
+        enter.config(show='*')
 
 def checkPassword():
     if enter.get() == "":
         messagebox.showinfo("Error", "Please enter a password to check.")
     elif (len(enter.get())) < 12:
                 messagebox.showinfo("Error", "Please enter a password with 12 or more characters.")
+    elif (len(enter.get())) < 12:
+                messagebox.showinfo("Error", "Please enter a password with 12 or more characters.")
 
 header = tk.Label(interface, text="Booster Security+", font=("Arial", 25, "bold"))
 header.pack(ipadx=20, ipady=20)
 
-inputText = Label(interface, text="Please enter password to check:", font=("Arial", 12))
+inputText = Label(interface, text="Please enter a password to test the strength:", font=("Arial", 12))
 inputText.pack(ipadx=10, ipady=10)
 
-enter = tk.Entry(interface)
+enter = tk.Entry(interface, show="*")
 enter.pack(ipadx=10, ipady=10)
 
+hideAndShow = Checkbutton(interface, text="show password", command=showPassword)
+hideAndShow.pack()
+
 enterButton = Button(interface, text="Check Password", font=("Arial", 12), command=checkPassword)
-enterButton.pack(ipadx=5, ipady=5)
+enterButton.pack()
 
 interface.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-"""
-# String Variable Initialization
-rand = StringVar()
-Msg = StringVar()
-key = StringVar()
-mode = StringVar()
-Result = StringVar()
-
-
-# Function that makes the reset button set all the String Values to nothing.
-def Reset():
-    rand.set("")
-    Msg.set("")
-    key.set("")
-    mode.set("")
-    Result.set("")
-
-
-# Labels
-
-textInp = Label(Interface, font = ('arial', 20, 'bold'),
-                text="Enter your Plaintext:", bd = 10, anchor = "w")
-
-textInp.grid(row = 0, column = 0)
-
-textOutLbl = Label(encoder, font = ('arial', 20, 'bold'),
-                   text="Results:", bd = 10, anchor = "w")
-
-textOutLbl.grid(row = 4, column = 0)
-
-# Text Input
-plainText = Entry(encoder, font = ('arial', 16, 'bold'),
-                textvariable = rand, bd = 10, insertwidth = 6,
-                justify = 'left')
-
-plainText.grid(row = 0 , column = 1)
-
-textOut = Entry(encoder, font = ('arial', 16, 'bold'),
-                textvariable = Result, bd = 10, insertwidth = 6,
-                justify = 'left')
-
-textOut.grid(row = 4, column = 1)
-
-Interface.mainloop()"""
